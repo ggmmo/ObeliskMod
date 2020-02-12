dab5209f867bdd5ebd5da28565323c183745b387
Kristina Verma <kristinaverma01@gmail.com>
2020-02-12 12:56:08 +1100

    minor changes to longtable class

--- Obelisk/src/main/java/fox/obeliskmod/blocks/LongTable.java
+++ Obelisk/src/main/java/fox/obeliskmod/blocks/LongTable.java
@@ -2,6 +2,8 @@ package fox.obeliskmod.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -9,9 +11,9 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 
-public class LongTable extends Block
+public class LongTable extends HorizontalBlock
 {
-	public static final VoxelShape Table_Body = Block.makeCuboidShape(0, 14, 0, 16, 16, 32);
+	public static final VoxelShape Table_Body = Block.makeCuboidShape(0, 0, 0, 16, 16, 32);
 	
 
 	public LongTable(Block.Properties properties) 
@@ -27,6 +29,10 @@ public class LongTable extends Block
 	 
 	 public BlockRenderLayer getRenderLayer() {
 	      return BlockRenderLayer.CUTOUT;
+	   }
+	 
+	 protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+	      builder.add(HORIZONTAL_FACING);
 	   }
 
 }
